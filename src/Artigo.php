c585eca5713dd4f41e55fc8cf905d530e578c2e7
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2022-12-15 10:46:20 -0300

    feat: add post and redirection

--- src/Artigo.php
+++ src/Artigo.php
@@ -12,6 +12,11 @@ class Artigo {
         return $artigos;
     }
 
+    public function adicionar(string $titulo, string $conteudo): void{
+        $insereArtigo = $this->mysql->prepare('INSERT INTO artigos (titulo, conteudo) VALUES (?, ?)');
+        $insereArtigo->bind_param('ss', $titulo, $conteudo);
+        $insereArtigo->execute();
+    }
     public function encontrarPorId(string $id): array{
 
         $selecionaArtigo = $this->mysql->prepare('SELECT id, titulo, conteudo FROM artigos WHERE id = ?');
